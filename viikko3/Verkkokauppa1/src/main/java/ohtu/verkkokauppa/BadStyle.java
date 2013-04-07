b2ccf55125169d51b67fdf709045f1ef63827b26
Markus <markus.nousiainen@cs.helsinki.fi>
2013-04-07 12:42:27 +0300

    tehtävä 6

---
+++ viikko3/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/BadStyle.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.verkkokauppa;
+
+/**
+ *
+ * @author markus
+ */
+public class BadStyle {
+    public void badMethod(){
+        if (1==1){
+        if (2==2){
+        if (3==3){
+        for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 3; j++){
+        for (int k = 0; k < 5; k++)
+        System.out.println("huonoa koodia");
+        }
+        }
+        }
+        }
+        }
+    }
+    
+    public void badMethod2(){
+        if (1==1){
+        if (2==2){
+        if (3==3){
+        for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 3; j++){
+        for (int k = 0; k < 5; k++)
+        System.out.println("huonoa koodia");
+        }
+        }
+        }
+        }
+        }
+    }
+}
